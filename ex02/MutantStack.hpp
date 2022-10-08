b98ec2c34ab0cbe1fb7953dbb6045a54a9e8070f
ymori <yuu.mori06@gmail.com>
2022-10-08 13:09:10 +0900

    ex02: refactor: reformat code

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -9,27 +9,29 @@
 
 template<class T>
 class MutantStack : public std::stack<T> {
-public:
-	MutantStack() : std::stack<T>() {};
-	MutantStack(const MutantStack &mutantStack) : std::stack<T>(mutantStack) {} ;
-	MutantStack &operator=(MutantStack &mutantStack) { return std::stack<T>::operator=(mutantStack); };
-	~MutantStack(){};
+ public:
+  MutantStack() : std::stack<T>() {};
+  MutantStack(const MutantStack &mutantStack) : std::stack<T>(mutantStack) {};
+  MutantStack &operator=(MutantStack &mutantStack) {
+    return std::stack<T>::operator=(mutantStack);
+  };
+  ~MutantStack() {};
 
-	typedef typename std::stack<T>::container_type::iterator iterator;
-	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+  typedef typename std::stack<T>::container_type::iterator iterator;
+  typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-	iterator    begin(void) {
-		return this->c.begin();
-	}
-	iterator    end(void) {
-		return this->c.end();
-	}
-	const_iterator begin(void) const {
-		return this->c.begin();
-	}
-	const_iterator end(void) const {
-		return this->c.end();
-	}
+  iterator begin(void) {
+    return this->c.begin();
+  }
+  iterator end(void) {
+    return this->c.end();
+  }
+  const_iterator begin(void) const {
+    return this->c.begin();
+  }
+  const_iterator end(void) const {
+    return this->c.end();
+  }
 };
 
 //#include "MutantStack.tpp"
